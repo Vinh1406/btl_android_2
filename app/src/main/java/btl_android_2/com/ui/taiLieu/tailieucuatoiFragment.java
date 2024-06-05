94da1b9a88658196da57a19f530bfd2dfd86331e
trengzi <trangnguyen16022003@gmail.com>
2024-06-05 18:19:41 +0700

    tai lieu quan tri, trang chu quan tri

--- app/src/main/java/btl_android_2/com/ui/taiLieu/tailieucuatoiFragment.java
+++ app/src/main/java/btl_android_2/com/ui/taiLieu/tailieucuatoiFragment.java
@@ -11,20 +11,18 @@ import android.view.ViewGroup;
 import btl_android_2.com.R;
 
 
-public class fragment_tailieu extends Fragment {
+public class tailieucuatoiFragment extends Fragment {
 
-    public fragment_tailieu() {
+
+    public tailieucuatoiFragment() {
         // Required empty public constructor
     }
-
-
-
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tailieucuatoi, container, false);
+        return inflater.inflate(R.layout.fragment_dangban, container, false);
     }
 }
